7d75ae97e7bf57c4c661c0b0a9bf3caccf666c52
Prabir Ghosh <prabir1992@gmail.com>
2020-03-30 17:53:37 +0530

    #comment Add Map To Application

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 Vue.config.productionTip = false
 
